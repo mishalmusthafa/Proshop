0f4189007fb58f442f19a16b470d68ca6fc3e54e
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-08-21 14:42:28 +0530

    Connected database using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
 
+connectDB(); //connecting DB using mongoose;
 const app = express();
 app.get('/', (req, res) => {
     res.send('Api is running');
