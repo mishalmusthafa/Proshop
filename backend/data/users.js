21951c04e932872f39ae094d82fc69d59fa65465
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-08-22 12:54:17 +0530

    Prepared Sample Data for seeding (userdata and product sample data)

---
+++ backend/data/users.js
@@ -0,0 +1,13 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name:'Admin User',email:'admin@gmail.com',password:bcrypt.hashSync('123456',10),isAdmin:true,
+  },
+  {
+    name:'John Doe',email:'johndoe@gmail.com',password:bcrypt.hashSync('123456',10),isAdmin:false,
+  },
+  {
+    name:'Jane Doe',email:'janedoe@gmail.com',password:bcrypt.hashSync('123456',10),isAdmin:false,
+  },
+]
