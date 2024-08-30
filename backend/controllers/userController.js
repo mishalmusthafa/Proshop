973ca382dbc6c49f48978313e167038b1b9d20a2
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-08-30 11:59:44 +0530

    Used httpOnly Cookie for jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from '../middleware/asyncHandler.js';
+import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 
 // @desc Auth user & get Token
@@ -7,7 +8,20 @@ import User from '../models/userModel.js';
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+
     if (user && (await user.matchPassword(password))) {
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '30d',
+        });
+
+        //Set JWT as HTTP-Only cookie
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict',
+            maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+        });
+
         res.json({
             _id: user._id,
             name: user.name,
@@ -18,7 +32,6 @@ const authUser = asyncHandler(async (req, res) => {
         res.status(401);
         throw new Error('Invalid email or password');
     }
-    res.send('Auth user');
 });
 
 // @desc Register user
