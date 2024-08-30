fd80c9265161cdff3e100d1a949f6214094413b5
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-08-30 12:42:17 +0530

    create auth middleware and endpoints

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 const port = process.env.PORT || 5000;
 
 connectDB(); //connecting DB using mongoose;
@@ -13,8 +14,12 @@ app.get('/', (req, res) => {
     res.send('Api is running');
 });
 
+// Body Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
+app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
