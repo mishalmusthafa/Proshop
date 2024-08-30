2121dca713338b4acbd9e87dc219e0817e05f6a1
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-08-30 17:53:55 +0530

    Created the register user function and create saperate file for the generateToken function for not repeating the code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
-import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc Auth user & get Token
 // @routes POST /api/users/login
@@ -10,18 +10,9 @@ const authUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email });
 
     if (user && (await user.matchPassword(password))) {
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-            expiresIn: '30d',
-        });
+        generateToken(res, user._id);
 
-        //Set JWT as HTTP-Only cookie
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
-        });
-
+        // responding with user details
         res.json({
             _id: user._id,
             name: user.name,
@@ -38,14 +29,42 @@ const authUser = asyncHandler(async (req, res) => {
 // @routes POST /api/users/
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.send('Register User');
+    const { name, email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    if (user) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 });
 
 // @desc Logout User
 // @routes POST /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('Logout User');
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+    res.status(200).json({ message: 'Logged out successfully' });
 });
 
 // @desc Get user Profile
